1e5a9c55569d884ba9e5f553f22fe5bac2d8241d
hboudraa <bdrnet94@gmail.com>
2025-07-26 11:26:40 +0100

    add templates

--- Config/stockapp/apps.py
+++ Config/stockapp/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class StockaaConfig(AppConfig):
+class StockaapConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'stockaa'
+    name = 'stockapp'
